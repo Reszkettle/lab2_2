07de705a9295b52151de4e44af76ec9175db657e
Reszkettle <reszkettle@gmail.com>
2021-03-22 17:57:29 +0100

    Added test for case when both sequences are empty

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,9 +1,27 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
+    @Test
+    public void shouldReturnOneWhenBothSequencesAreEmpty() {
+        // given
+        SimilarityFinder finder = new SimilarityFinder((elem, sequence) -> null);
+        int[] firstSeq = {};
+        int[] secondSeq = {};
+
+        // when
+        double similarity = finder.calculateJackardSimilarity(firstSeq, secondSeq);
+
+        // then
+        assertEquals(1.0d, similarity);
+    }
 }
